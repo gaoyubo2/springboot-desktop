7d34d03f5dd689da32695fbd3e47033b5a039e69
cyl <ucascyl@gmail.com>
2024-03-20 16:36:51 +0800

    验证码相关接口完毕

--- src/test/java/cn/cest/os/sso/SsoApplicationTests.java
+++ src/test/java/cn/cest/os/sso/SsoApplicationTests.java
@@ -1,13 +1,21 @@
 package cn.cest.os.sso;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SsoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+//
+//    @Test
+//    void contextLoads() {
+//        //增
+//        redisTemplate.opsForValue().set("name","Test");
+//        String name = (String)redisTemplate.opsForValue().get("name");
+//        System.out.println(name);
+//    }
 
 }
